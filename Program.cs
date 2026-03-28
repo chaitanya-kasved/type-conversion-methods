357fdfc869a11edc08771dac2f7771d7144324ba
Chaitanya Kasved <kasvedc@gmail.com>
2026-03-28 03:42:21 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+namespace Program
+{
+	class code
+	{
+		static void Main(string[] args)
+		{
+			// Type casting Methods.
+			char L = 'L';
+		    int age = 17;
+			bool canMove = false;
+		    float num = 3.14F;
+
+			Console.WriteLine(Convert.ToInt32(L)); // Converts To int
+			Console.WriteLine(Convert.ToString(age)); // Converts To string
+			Console.WriteLine(Convert.ToInt32(canMove)); // Converts To int
+			Console.WriteLine(Convert.ToInt32(num)); // Converts To int
+		}
+	}
+}
